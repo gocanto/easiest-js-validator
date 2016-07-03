b0d89699e9c2a153c96cdeeb5c5c0548f5710dcc
Gustavo Ocanto <gustavoocanto@gmail.com>
2016-07-03 12:49:36 -0500

    Demo + gulp

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var elixir = require('laravel-elixir');
+
+elixir.config.sourcemaps = false;
+elixir.config.assetsPath = 'src';
+
+elixir(function(mix)
+{
+	mix.browserify('index.js', 'dist/easiest-js-validator.js');
+});
